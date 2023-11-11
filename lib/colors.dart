d30afe1866330bf9269b9b466af0873d4ee2a174
HerrAlpha <90854039+HerrAlpha@users.noreply.github.com>
2023-11-11 21:28:31 +0700

    First Commit Colors
    For Apreciated and finish task of issue #2 to:
    
    - M. Arfan Aryasatya
    - Mohammad Agung Rizki (Owner)
    - Muhammad Naufal Farras
    - Muhammad Yusuf Alvianto
    - Rasendriya Ahmada Maheswara
    - Shohwatul Hana

---
+++ lib/colors.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class FluttilColors {
+  FluttilColors._();
+
+  // static const Color namaWarna = Color.fromRGBO(R, G, B, 1);
+
+// Business:
+  static const Color businessBlue = Color.fromRGBO(0, 145, 213, 1);
+  static const Color businessWhite = Color.fromRGBO(241, 241, 241, 1);
+  static const Color businessNavy = Color.fromRGBO(28, 78, 128, 1);
+  static const Color businessAqua = Color.fromRGBO(165, 216, 221, 1);
+  static const Color businessBlack = Color.fromRGBO(35, 40, 45, 1);
+  static const Color businessCyan = Color.fromRGBO(76, 181, 245, 1);
+  static const Color businessDarkBlue = Color.fromRGBO(31, 63, 73, 1);
+  static const Color businessGrey = Color.fromRGBO(206, 210, 204, 1);
+  static const Color businessGreen = Color.fromRGBO(106, 177, 135, 1);
+  static const Color businessRed = Color.fromRGBO(234, 106, 71, 1);
+
+// Health:
+  static const Color healthGrey = Color.fromRGBO(199, 206, 244, 1);
+  static const Color healthBlue = Color.fromRGBO(97, 170, 197, 1);
+  static const Color healthGreen = Color.fromRGBO(181, 220, 221, 1);
+  static const Color healthPurple = Color.fromRGBO(210, 122, 231, 1);
+  static const Color healthYellow = Color.fromRGBO(248, 239, 212, 1);
+
+// E-commerce:
+  /// General:
+  static const Color comGeneralBlack = Color.fromRGBO(51, 42, 41, 1);
+  static const Color comGeneralDarkBrown = Color.fromRGBO(71, 46, 43, 1);
+  static const Color comGeneralBrown = Color.fromRGBO(122, 56, 49, 1);
+  static const Color comGeneralDarkRed = Color.fromRGBO(173, 49, 35, 1);
+  static const Color comGeneralRed = Color.fromRGBO(225, 24, 0, 1);
+
+  /// Furnitur atau Pakaian:
+  static const Color comFurnClothesGrey = Color.fromRGBO(173, 168, 156, 1);
+  static const Color comFurnClothesDarkGrey = Color.fromRGBO(122, 111, 86, 1);
+  static const Color comFurnClothesLightYellow =
+      Color.fromRGBO(224, 204, 157, 1);
+  static const Color comFurnClothesBrown = Color.fromRGBO(71, 61, 36, 1);
+  static const Color comFurnClothesDarkBrown = Color.fromRGBO(51, 39, 11, 1);
+
+  /// Kosmetik:
+  static const Color comCosmeticDarkBrown = Color.fromRGBO(51, 19, 17, 1);
+  static const Color comCosmeticBrown = Color.fromRGBO(69, 35, 33, 1);
+  static const Color comCosmeticPink = Color.fromRGBO(222, 164, 160, 1);
+  static const Color comCosmeticLightBrown = Color.fromRGBO(120, 84, 81, 1);
+  static const Color comCosmeticGrey = Color.fromRGBO(171, 152, 150, 1);
+
+  /// Furnitur:
+  static const Color comFurnitureNavy = Color.fromRGBO(15, 26, 51, 1);
+  static const Color comFurnitureLightNavy = Color.fromRGBO(36, 46, 71, 1);
+  static const Color comFurnitureLightBlue = Color.fromRGBO(156, 176, 224, 1);
+  static const Color comFurnitureGrey = Color.fromRGBO(86, 97, 122, 1);
+  static const Color comFurnitureLightGrey = Color.fromRGBO(156, 161, 173, 1);
+}
